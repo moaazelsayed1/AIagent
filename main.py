abb7e055bac262c26827870b6d3c0a41e4b111fa
moaaz <moaz.elsayed002@gmail.com>
2025-09-04 10:54:57 +0200

    add the prompt from the command line

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from aiagent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+if(len(sys.argv) < 2):
+    print("Usage: script_name promprt")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+)
+print("Prompt tokens:",  response.usage_metadata.prompt_token_count)
+print("Response tokens:",  response.usage_metadata.candidates_token_count)
