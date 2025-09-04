a71e87327481376a6ef7f226081ba2ea078bf714
moaaz <moaz.elsayed002@gmail.com>
2025-09-04 11:02:14 +0200

    add --verbose option

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 from google.genai import types
 
 if(len(sys.argv) < 2):
-    print("Usage: script_name promprt")
+    print("Usage: script_name promprt --[verbose]")
     sys.exit(1)
 
 load_dotenv()
@@ -13,12 +13,15 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
 ]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents=messages,
 )
-print("Prompt tokens:",  response.usage_metadata.prompt_token_count)
-print("Response tokens:",  response.usage_metadata.candidates_token_count)
+if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
+    print("User prompt:", sys.argv[1])
+    print("Prompt tokens:",  response.usage_metadata.prompt_token_count)
+    print("Response tokens:",  response.usage_metadata.candidates_token_count)
+
